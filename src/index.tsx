154a4da63157f8a652ae189c0d7ca06fdeed9ba0
Robin Hjelmeir <hjelmeir@gmail.com>
2021-04-01 22:49:50 -0400

    Pass MenuProps to child <Menu/> item

--- src/index.tsx
+++ src/index.tsx
@@ -171,6 +171,7 @@ const NestedMenuItem = React.forwardRef<
         {rightIcon}
       </MenuItem>
       <Menu
+        {...MenuProps}
         // Set pointer events to 'none' to prevent the invisible Popover div
         // from capturing events for clicks and hovers
         style={{pointerEvents: 'none'}}
